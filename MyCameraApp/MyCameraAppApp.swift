cb0d4711cf282ca6d8746e79d06536972308e711
amefure <shachi10969@gmail.com>
2024-04-17 21:07:21 +0900

    Initial Commit

---
+++ MyCameraApp/MyCameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraAppApp.swift
+//  MyCameraApp
+//
+//  Created by t&a on 2024/04/17.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
